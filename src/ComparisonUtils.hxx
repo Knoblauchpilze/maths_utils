a1dd8a06a109f6aa7062bb1b7140331f92a76dc7
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2019-07-07 23:18:01 +0200

    Added specialization of 'fuzzyEqual' for int as we can provide exact equality test

--- src/ComparisonUtils.hxx
+++ src/ComparisonUtils.hxx
@@ -6,12 +6,23 @@
 namespace utils {
 
   template <typename DataType>
+  inline
   bool
   fuzzyEqual(const DataType& value1,
              const DataType& value2,
              const DataType& epsilon)
   {
     return std::abs(value1 - value2) < epsilon;
+  }
+
+  template <>
+  inline
+  bool
+  fuzzyEqual(const int& value1,
+             const int& value2,
+             const int& epsilon)
+  {
+    return value1 == value2;
   }
 
 }
